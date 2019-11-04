15000c450bbd2a9511bd76f8f68b1863ad1d9577
Eliane-5 <irankundaeliane9@gmail.com>
2019-11-04 15:36:02 +0200

    Implementing onclick listener

--- app/src/main/java/com/moringaschool/craftypictures/TakePicture.java
+++ app/src/main/java/com/moringaschool/craftypictures/TakePicture.java
@@ -3,13 +3,14 @@ package com.moringaschool.craftypictures;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class TakePicture extends AppCompatActivity {
+public class TakePicture extends AppCompatActivity implements View.OnClickListener{
     @BindView(R.id.pictureButton) Button mPictureButton;
     @BindView(R.id.capturedImage) ImageView mCapturedImage;
 
@@ -18,6 +19,11 @@ public class TakePicture extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_picture);
         ButterKnife.bind(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
