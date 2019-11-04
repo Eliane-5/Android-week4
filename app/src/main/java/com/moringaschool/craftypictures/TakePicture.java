fc9439905770fb6c2c0049fc811870caaa3a71b0
Eliane-5 <irankundaeliane9@gmail.com>
2019-11-04 15:46:38 +0200

    Setting an intent to tell the user what button does

--- app/src/main/java/com/moringaschool/craftypictures/TakePicture.java
+++ app/src/main/java/com/moringaschool/craftypictures/TakePicture.java
@@ -2,7 +2,9 @@ package com.moringaschool.craftypictures;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -32,6 +34,6 @@ public class TakePicture extends AppCompatActivity implements View.OnClickListen
     }
 
     private void takePictureAction() {
-
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
     }
 }
