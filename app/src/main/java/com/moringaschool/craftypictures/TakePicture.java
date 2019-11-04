46b128cd0fe463fd2e3d7eb306529761d95d755a
Eliane-5 <irankundaeliane9@gmail.com>
2019-11-04 15:25:13 +0200

    Binding the images and buttons to java folder

--- app/src/main/java/com/moringaschool/craftypictures/TakePicture.java
+++ app/src/main/java/com/moringaschool/craftypictures/TakePicture.java
@@ -3,12 +3,21 @@ package com.moringaschool.craftypictures;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class TakePicture extends AppCompatActivity {
+    @BindView(R.id.pictureButton) Button mPictureButton;
+    @BindView(R.id.capturedImage) ImageView mCapturedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_picture);
+        ButterKnife.bind(this);
+
     }
 }
